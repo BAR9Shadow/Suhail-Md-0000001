703651e109780f0ef5fddc4e541020cbbf06eb5e
Saban_AI <129470870+BAR9Shadow@users.noreply.github.com>
2024-07-30 18:33:41 +0100

    Update config.js

--- config.js
+++ config.js
@@ -92,7 +92,7 @@ module.exports = {
 
 
   WORKTYPE: process.env.WORKTYPE || process.env.MODE|| "private",
-  LANG: ( process.env.THEME ||  "x2Z"  ).toUpperCase(),
+  LANG: ( process.env.THEME ||  "Monkey-D-Luffy"  ).toUpperCase(),
 
 
 
